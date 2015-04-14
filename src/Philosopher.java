316f13962c3ae73dcfd773485aaabf09ac402e8b
deangarcia <steel34@live.com>
2015-04-14 09:35:29 -0700

    another attempt
    same concept except instead a philosopher in this program is defined as
    starving if another philosopher has eaten 5 times more than him.

--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,44 +1,47 @@
+import java.util.Random;
 import java.util.concurrent.locks.*;
 public class Philosopher extends Thread {
     private static int WAITING=0, EATING=1, THINKING=2;
     private Lock lock;
     private Condition phil [];
     private int states [];
-    private int eatCount [];
+    private static int eatCount [];
     private int NUM_PHILS;
     private int id;
     private final int TURNS = 20;
-    private static int phase = 0;
-    private static int phaseCount = 0;
+    private static int starving = 0;
     public Philosopher (Lock l, Condition p[], int st[], int num, int id, int ec[]) {
         lock = l; phil = p; states = st; this.id = id; eatCount = ec;
         NUM_PHILS = num;
     }
     public void run () {
         for (int k=0; k<TURNS; k++) {
-        try { sleep(100); } catch (Exception ex) { /* lazy */}
+        //try { sleep(100); } catch (Exception ex) { /* lazy */}
         takeSticks(id); 
         try { sleep(20); } catch (Exception ex) { }
         putSticks(id);
+        output();
         }
     }
+    
     public boolean checkPhase(int eatC)
     {
-    	if(phase == eatC)
+    	int hungriest = eatC;
+    	int hungryPos = 0;
+    	for(int i = 0; i<eatCount.length; i++)
     	{
-    		phaseCount++;
-    		if(phaseCount % NUM_PHILS == 0)
+    		if(eatCount[i] < hungriest)
     		{
-    			updatePhase();
+    			hungriest = eatCount[i];
+    			hungryPos = i;
     		}
-    		return true;
     	}
-    	else 
-    		return false;
-    }
-    public void updatePhase()
-    {
-    	phase +=1;
+    	if((Math.abs((eatC - hungriest)) % 5 == 0) && eatC > hungriest)
+		{
+    		starving = hungryPos;
+			return false;
+		}
+    	return true;
     }
     public void takeSticks (int id) {
         lock.lock();
@@ -62,7 +65,22 @@ public class Philosopher extends Thread {
     public void output() {
         lock.lock();
         for (int k=0; k<states.length; k++)
-            System.out.print(states[k]+",");
+        {
+        	if(states[k] == 0)
+        	{
+        		System.out.println("P" + k + " is Waiting " + ", he has Eaten " + eatCount[k] + " times");
+        	}
+        	else if(states[k] == 1)
+        	{
+        		System.out.println("P" + k + " is Eating " + ", he has Eaten " + eatCount[k] + " times");
+        	}
+        	else
+        	{
+        		System.out.println("P" + k + " is Thinking " + ", he has Eaten " + eatCount[k] + " times");
+        	}
+        }
+        System.out.println("Phase Over");
+            
         lock.unlock();
         System.out.println();
     }
